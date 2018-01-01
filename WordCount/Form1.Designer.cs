1158ac3c34497636649f8532035324067a8ca62d
ProgramerMyFirstChoice <programer2018@gmail.com>
2018-01-01 10:33:29 -0800

    File

---
+++ WordCount/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WordCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnWordCount = new System.Windows.Forms.Button();
+            this.txtWordCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter String To Display Number of Words:";
+            // 
+            // btnWordCount
+            // 
+            this.btnWordCount.Location = new System.Drawing.Point(105, 118);
+            this.btnWordCount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnWordCount.Name = "btnWordCount";
+            this.btnWordCount.Size = new System.Drawing.Size(202, 36);
+            this.btnWordCount.TabIndex = 1;
+            this.btnWordCount.Text = "Word Count";
+            this.btnWordCount.UseVisualStyleBackColor = true;
+            this.btnWordCount.Click += new System.EventHandler(this.btnWordCount_Click);
+            // 
+            // txtWordCount
+            // 
+            this.txtWordCount.Location = new System.Drawing.Point(42, 61);
+            this.txtWordCount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtWordCount.Name = "txtWordCount";
+            this.txtWordCount.Size = new System.Drawing.Size(434, 26);
+            this.txtWordCount.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 167);
+            this.Controls.Add(this.txtWordCount);
+            this.Controls.Add(this.btnWordCount);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Display Word Count";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnWordCount;
+        private System.Windows.Forms.TextBox txtWordCount;
+    }
+}
+
---
+++ WordCount/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWordCount_Click(object sender, EventArgs e)
+        {
+            // Assign entered string in str variable
+            string str = txtWordCount.Text;
+
+            // Trim the string in both trailing and leading space from the string
+            if (str.Trim() == "")
+            {
+                MessageBox.Show("Please enter the string to display number of word", "Display Word Count Error");
+                // It would not excute the remaining code
+                return;
+            }
+
+            int noOfWords = StringWordsCount(str);
+
+            MessageBox.Show("Number of words in the given string is: " + noOfWords.ToString());
+
+        }
+
+
+        private int StringWordsCount(string str)
+        {
+            int wordsCount = 0;
+
+            string[] strWords = str.Split(null);
+            wordsCount = strWords.Length;
+            return wordsCount;
+        }
+    }
+}
